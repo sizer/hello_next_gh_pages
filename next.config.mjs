58cde9f62a294ffefbb40f426d3a47ae5c028dd2
sizer <shinse.tanaka@gmail.com>
2024-06-01 21:59:33 +0900

    implement GitHub Page hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isGhActions = !!process.env.GITHUB_ACTIONS;
+
 const nextConfig = {
     output: 'export',
+    assetPrefix: isGhActions ? `https://sizer.github.io/hello_next_gh_pages/` : '',
+    basePath: isGhActions ? '/hello_next_gh_pages' : '',
 };
 
 export default nextConfig;
